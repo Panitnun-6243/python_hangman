3eaef68400e78cbe4afdf83c66ea32c76a2ea5ff
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-20 21:15:17 +0000

    Add dash

--- main.py
+++ main.py
@@ -11,22 +11,36 @@ print(art.logo)
 print(chosen_word)
 print(chosen_word_list)
 guess_letter = ''
+display = []
+for _ in range(len(chosen_word_list)):
+    display += "_"
 #loop letter
 while num_incorrect_word < penalty_limit_point and len(chosen_word_list) > 0:
   guess_letter = input("Guess a letter: ")[0].lower()
   num_right_guess = 0
+  
   #loop check all item in list
   for letter in chosen_word_list:
     if letter == guess_letter and len(chosen_word_list) > 0:
+      for position in range(len(chosen_word_list)):
+        letter = chosen_word[position]
+        if letter == guess_letter:
+          display[position] = letter
       num_right_guess += 1
       if num_right_guess > 0:
         chosen_word_list.remove(guess_letter)
+      print()
+      print(display)
+      print()
+      # if art_stages < 6:
+      #   print(art.stages[art_stages])
+      print(f"You guess {guess_letter}, that's in the word!\n")
       
   if num_right_guess == 0:
     print()
-    print("- "*20)
+    print(display)
+    print()
     print(f"You guess {guess_letter}, that's not in the word. You lose a life.")
-    
     print(art.stages[art_stages])
     art_stages -= 1
     num_incorrect_word += 1
