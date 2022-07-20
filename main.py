7455babf8722de19398a4310901d3f97ad988505
Panitnun Suvannabun <Panitnun.6243@gmail.com>
2022-07-20 18:31:31 +0000

    Add logic structure

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print(6)
+import hangman_words as words
+import hangman_art as art
+import random
+penalty_limit_point = 7
+random_num = random.randint(0,212)
+chosen_word = words.word_list[random_num]
+chosen_word_list = list(chosen_word)
+print(art.logo)
+print(chosen_word)
+print(chosen_word_list)
+guess_letter = []
+for i in range(0, len(chosen_word_list)):
+  input("Guess a letter: ")[0].lower()
+print(guess_letter)
